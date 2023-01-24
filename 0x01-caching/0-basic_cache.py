e6ce1259050590215c51ddd2a243092ce2c2a439
LouisGichiah <louiegpaul@gmail.com>
2023-01-24 21:28:45 +0300

    redone

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-""" Module for class BasicCache """
-
+""" Python caching systems """
 from base_caching import BaseCaching
 
-class BasicCache(BaseCaching):
-    """ inherits from BaseCaching and is a caching system """
 
+class BasicCache(BaseCaching):
+    """ Dict caching system """
     def put(self, key, item):
-        """
-        Add an item in the cache
-        """
+        """ Add an item in the cache """
         if key is None or item is None:
             pass
-
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Get an item by key
-        """
-        if key is None:
-            None
-
-        elif key not in self.cache_data:
-            None
-
-        else:
-            return self.cache_data.get(key)
+        """ Get an item by key """
+        return self.cache_data.get(key)
